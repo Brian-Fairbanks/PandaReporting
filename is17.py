9dd7bad6b2d6fd404496599d06726120581fddc3
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2021-10-21 16:00:17 -0500

    included district 17 - included shape file - set up sample to show if a passed point is in the shape files boundaries

--- is17.py
+++ is17.py
@@ -1,23 +1,15 @@
-from shapely.geometry import MultiPolygon
+# from shapely.geometry import MultiPolygon
+from shapely.geometry import Point, Polygon, MultiPolygon
 import geopandas as gpd
 
+esd17 = gpd.read_file("esd17.shp")
 
-def points_from_polygons(polygons):
-    points = []
-    for mpoly in polygons:
-        if isinstance(mpoly, MultiPolygon):
-            polys = list(mpoly)
-        else:
-            polys = [mpoly]
-        for polygon in polys:
-            for point in polygon.exterior.coords:
-                points.append(point)
-            for interior in polygon.interiors:
-                for point in interior.coords:
-                    points.append(point)
-    return points
+# specify that source data is 'NAD 1983 StatePlane Texas Central FIPS 4203 (US Feet)' - https://epsg.io/2277
+esd17.set_crs(epsg=2277, inplace=True)
+# and convert to 'World Geodetic System 1984' (used in GPS) - https://epsg.io/4326
+esd17 = esd17.to_crs(4326)
 
-
-points = points_from_polygons(habitat.geometry)
-x = [point.x for point in points]
-y = [point.y for point in points]
+p1 = Point(-97.60254748509955, 30.398634794025643)
+print(
+    (esd17.contains(p1)).any()
+)  # check if this point is in any of the polygons included in the shape file
