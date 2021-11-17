52123e5d8e681c7a1872a4bc13513570e38a8d26
Brian <brian.k.fairbanks@gmail.com>
2021-11-17 16:45:24 -0600

    extracting population density/classification data into the fireDF

--- analyzefire.py
+++ analyzefire.py
@@ -314,18 +314,25 @@ def analyzeFire(fireDF):
     # =================================================================
     print("loading population grid:")
     popData = gpd.read_file("Shape\\ESD2Pop.shp")
-    # specify that source data is 'NAD 1983 StatePlane Texas Central FIPS 4203 (US Feet)' - https://epsg.io/2277
-    popData.set_crs(epsg=2277, inplace=True)
+    # specify that source data is WGS 84 / Pseudo-Mercator -- Spherical Mercator, Google Maps, OpenStreetMap, Bing, ArcGIS, ESRI' - https://epsg.io/3857
+    popData.set_crs(epsg=3857, inplace=True)
     # and convert to 'World Geodetic System 1984' (used in GPS) - https://epsg.io/4326
     popData = popData.to_crs(4326)
 
-    # print(mapsco)
+    # weird aliases... this is total population / AreaofLAND(meters)
+    popData["Pop/Mile"] = popData.apply(
+        lambda x: (x["B01001_001"] / x["ALAND"]) * 2590000, axis=1
+    )
 
     def getPopulationDensity(lon, lat):
         plot = Point(lon, lat)
         try:
-            return popData[popData.contains(plot)]
-            # return popData.loc[(popData.index[popData.contains(plot)])[0], "MAPSCO_PAG"]
+            # return None
+            mapInd = (popData.index[popData.contains(plot)])[0]
+            # population = popData.loc[mapInd, "B01001_001E"]
+            # area = popData.loc[mapInd, "Shape_Area"]
+            return popData.loc[mapInd, "Pop/Mile"]
+            # return mapInd
         except:
             return None
 
@@ -335,9 +342,20 @@ def analyzeFire(fireDF):
         desc=f"finding Population Data:",
         leave=False,
     )
-    fireDF["Population Density"] = fireDF.progress_apply(
+    fireDF["People/Mile"] = fireDF.progress_apply(
         lambda x: getPopulationDensity(x["X-Long"], x["Y_Lat"]),
         axis=1,
+    )
+
+    def popRatio(pop):
+        if pop < 1000:
+            return "rural"
+        if pop < 2000:
+            return "suburban"
+        return "urban"
+
+    fireDF["Population Classification"] = fireDF.apply(
+        lambda x: popRatio(x["People/Mile"]), axis=1
     )
 
     # (getMapscoGrid)(fireDF["X-Long"], fireDF["Y_Lat"])
