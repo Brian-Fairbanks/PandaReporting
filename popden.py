a0519d42b41d6270dc2fbc184a6826b733431a4c
Brian <brian.k.fairbanks@gmail.com>
2021-12-01 14:59:19 -0600

    closes #24 Null densitys now return Outisde ESD2 additionally capitalized results

--- popden.py
+++ popden.py
@@ -1,4 +1,5 @@
 from shapely.geometry import Point
+import pandas as pd
 import geopandas as gpd
 
 
@@ -10,7 +11,7 @@ def addPopDen(fireDF):
     # and convert to 'World Geodetic System 1984' (used in GPS) - https://epsg.io/4326
     popData = popData.to_crs(4326)
 
-    # weird aliases... this is total population / AreaofLAND(meters)
+    # weird aliases... this is 'total population' / 'AreaofLAND(meters)'
     popData["Pop/Mile"] = popData.apply(
         lambda x: (x["B01001_001"] / x["ALAND"]) * 2590000, axis=1
     )
@@ -18,12 +19,8 @@ def addPopDen(fireDF):
     def getPopulationDensity(lon, lat):
         plot = Point(lon, lat)
         try:
-            # return None
             mapInd = (popData.index[popData.contains(plot)])[0]
-            # population = popData.loc[mapInd, "B01001_001E"]
-            # area = popData.loc[mapInd, "Shape_Area"]
             return popData.loc[mapInd, "Pop/Mile"]
-            # return mapInd
         except:
             return None
 
@@ -39,11 +36,13 @@ def addPopDen(fireDF):
     )
 
     def popRatio(pop):
+        if pd.isnull(pop):
+            return "Outside ESD2"
         if pop < 1000:
-            return "rural"
+            return "Rural"
         if pop < 2000:
-            return "suburban"
-        return "urban"
+            return "Suburban"
+        return "Urban"
 
     fireDF["Population Classification"] = fireDF.apply(
         lambda x: popRatio(x["People/Mile"]), axis=1
